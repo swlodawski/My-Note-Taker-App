c9428fbd7c39f52749233a6e003c9bad118269b6
sammy wlodawski <samwlodawski@gmail.com>
2024-08-06 21:28:18 -0400

    Created new note store function to store, update and delete new notes.

--- db/store.js
+++ db/store.js
@@ -10,6 +10,41 @@ class Store {
     };
 
     getNotes() {
-        
+        return this.read().then((notes) => {
+            let getNotes;
+            if (notes === null) {
+                getNotes = [];
+            } else { getNotes = [].concat(JSON.parse(notes));
+            }
+            return getNotes;
+        });
+    };
+    addNotes(notes) {
+        const {text, title} = notes;
+        if(!text || !title) {
+            return;
+        };
+
+        const addedNote = {
+            text,
+            title,
+            id: uuidv4()
+        };
+        return this.getNotes().then((notes) => {
+            return[...notes, addedNote];
+        })
+        .then((updatedNote) => {
+            this.write(updatedNote);
+        })
+        .then(() => addedNote);
+    };
+
+    removeNotes(id) {
+        return this.getNotes.then((notes) => {
+            return notes.filter((note) => note.id !== id)
+        })
+        .then((filtered) => this.write(filtered))
     }
 }
+
+module.exports = Store();
