bbaa515056f4fe6ff9034bd3cccebd91f96825cc
sammy wlodawski <samwlodawski@gmail.com>
2024-08-20 11:41:53 -0400

    Added test code to verify functionaility in store and routes.

--- db/store.js
+++ db/store.js
@@ -1,27 +1,75 @@
 const fs = require('fs/promises');
 const {v4: uuidv4} = require('uuid');
 
+// class Store {
+//     read() {
+//         return fs.readFile('db/db.json', 'utf-8');
+//     };
+//     write(none) {
+//         return fs.writeFile('db/db/json', JSON.stringify(note));
+//     };
+
+//     getNotes() {
+//         return this.read().then((notes) => {
+//             let getNotes;
+//             if (notes === null) {
+//                 getNotes = [];
+//             } else { getNotes = [].concat(JSON.parse(notes));
+//             }
+//             return getNotes;
+//         });
+//     };
+//     addNotes(notes) {
+//         const {text, title} = notes;
+//         if(!text || !title) {
+//             return;
+//         };
+
+//         const addedNote = {
+//             text,
+//             title,
+//             id: uuidv4()
+//         };
+//         return this.getNotes().then((notes) => {
+//             return[...notes, addedNote];
+//         })
+//         .then((updatedNote) => {
+//             this.write(updatedNote);
+//         })
+//         .then(() => addedNote);
+//     };
+
+//     removeNotes(id) {
+//         return this.getNotes.then((notes) => {
+//             return notes.filter((note) => note.id !== id)
+//         })
+//         .then((filtered) => this.write(filtered))
+//     }
+// }
 class Store {
     read() {
-        return fs.readFile('db/db.json', 'utf-8');
+        return fs.readFile('db/db.json', 'utf8');
     };
-    write(none) {
-        return fs.writeFile('db/db/json', JSON.stringify(note));
+    write(note) {
+        return fs.writeFile('db/db.json', JSON.stringify(note));
     };
 
     getNotes() {
-        return this.read().then((notes) => {
-            let getNotes;
-            if (notes === null) {
-                getNotes = [];
-            } else { getNotes = [].concat(JSON.parse(notes));
-            }
-            return getNotes;
-        });
+        return this.read()
+            .then((notes) => {
+                let getNotes;
+                if (notes == null) {
+                    getNotes = [];
+                } else {
+                    getNotes = [].concat(JSON.parse(notes));
+                }
+                return getNotes;
+            });
     };
+
     addNotes(notes) {
         const {text, title} = notes;
-        if(!text || !title) {
+        if (!text || !title) {
             return;
         };
 
@@ -30,21 +78,23 @@ class Store {
             title,
             id: uuidv4()
         };
-        return this.getNotes().then((notes) => {
-            return[...notes, addedNote];
+
+        return this.getNotes()
+            .then((notes) => {
+                return [...notes, addedNote];
         })
-        .then((updatedNote) => {
-            this.write(updatedNote);
+            .then((updatedNote) => {
+                this.write(updatedNote);
         })
-        .then(() => addedNote);
+            .then(() => addedNote);
     };
 
     removeNotes(id) {
-        return this.getNotes.then((notes) => {
-            return notes.filter((note) => note.id !== id)
-        })
-        .then((filtered) => this.write(filtered))
+        return this.getNotes
+            .then((notes) => {
+                return notes.filter((note) => note.id !== id)
+            })
+            .then((filtered) => this.write(filtered))
     }
 }
-
 module.exports = new Store();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,28 +2,52 @@ const express = require('express');
 const router = express.Router();
 const store = require('../db/store');
 
+// router.get('/notes', (req, res) => {
+//     store.getNotes().then((notes) => {
+//         console.log(notes);
+//         return res.json(notes);
+//     })
+//     .catch((err) => {
+//         res.status(500).json(err);
+//     }) 
+// })
+// router.post('/notes', (req, res) => {
+//     store.addNotes(req.body).then((note) => {
+//         console.log(note);
+//         return res.json(note);
+//     })
+//     .catch((err) => {
+//         res.status(500).json(err);
+//     }) 
+// })
+// router.delete('/notes/:id', (req, res) => {
+//     store.removeNotes(req.params.id).then(() => {
+//         res.json({ok: true})
+//     })
+// })
 router.get('/notes', (req, res) => {
-    store.getNotes().then((notes) => {
-        console.log(notes);
-        return res.json(notes);
-    })
-    .catch((err) => {
-        res.status(500).json(err);
-    }) 
+    store.getNotes()
+        .then((notes) => {
+            console.log(notes);
+            return res.json(notes);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        })
 })
+
 router.post('/notes', (req, res) => {
-    store.addNotes(req.body).then((note) => {
-        console.log(note);
-        return res.json(note);
-    })
-    .catch((err) => {
-        res.status(500).json(err);
-    }) 
+    store.addNotes(req.body)
+        .then((note) => {
+            return res.json(note);
+        })
 })
+
 router.delete('/notes/:id', (req, res) => {
-    store.removeNotes(req.params.id).then(() => {
-        res.json({ok: true})
-    })
+    store.removeNotes(req.params.id)
+        .then(() => res.json({
+            ok: true
+        }))
 })
 
 module.exports = router;
